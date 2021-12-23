d7257158ffb5a8a30a6887768731462c854d77ea
youssouf Abdouraouf <youssouf.abdouraouf7@gmail.com>
2021-12-23 10:19:21 +0100

    pagination

--- src/main/java/doc/raf/controllers/ProduitContoller.java
+++ src/main/java/doc/raf/controllers/ProduitContoller.java
@@ -3,9 +3,12 @@ package doc.raf.controllers;
 import doc.raf.dao.ProduitReposotory;
 import doc.raf.entities.Produits;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,7 +19,9 @@ public class ProduitContoller {
     @GetMapping(value = "/index")
     public String getAllProduits(Model model){
         List<Produits> produits = prodRepo.findAll();
-        model.addAttribute("ListeProduit",produits); // ListeProduit c'est ce qui fait le refferencent avec thymeleaf pour afficher les produits
+        model.addAttribute("lesProduits", produits);
+       //ListeProduit c'est ce qui fait le refferencent avec thymeleaf pour afficher les produits TOUT comme ce qui est dans addAttribute
         return "produit";
+       
     }
 }
